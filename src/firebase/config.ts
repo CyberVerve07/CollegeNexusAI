5e95f3b70d6db5b4eadb28ffea5f2186dc362a92
Aditya Mahajan <adityamahajan7287@gmail.com>
2025-11-29 20:16:07 +0530

    Remove firebase configuration details

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,1 @@
-export const firebaseConfig = {
-  "projectId": "studio-2892584014-2bd2b",
-  "appId": "1:719567871074:web:b526bdcf667b6a507b053b",
-  "apiKey": "AIzaSyCQvSZUM7aEpYnIDW6hDH9C3qgdSngo2CY",
-  "authDomain": "studio-2892584014-2bd2b.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "719567871074"
-};
+Secrate h bhai 
